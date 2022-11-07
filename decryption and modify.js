deec764f06b10f961d95c78430741359b6615d7e
HONGGU83 <117610356+HONGGU83@users.noreply.github.com>
2022-11-07 10:25:36 +0900

    Add files via upload

---
+++ decryption and modify.js
@@ -0,0 +1,21 @@
+setImmediate(function(){
+	Java.perform(function(){
+
+		var decrypteClass = Java.use("test.test.test")
+		decrypteClass.a.implementation = function(arg1, arg2){
+			var secret_string = this.a(arg1, arg2);
+			var secret_S = ''
+			for(var i=0; i<secret_string.length; i++)
+			{
+				secret_S += String.fromCharCode(secret_string[i])
+			}
+			console.log("[*] Decrypted : "+secret_S);
+			
+			if(secret_S .indexOf('I want to believe')>=0){
+				secret_S =secret_S .replace('I want to believe','modify success');
+			}
+			console.log("[*] modify : "+secret_S);
+			return secret_string;
+		}
+	})
+})
